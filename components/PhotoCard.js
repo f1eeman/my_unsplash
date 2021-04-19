755938d0cd5e9fff119dbab2bed2dad3513f6266
f1eeman <mr.f1eeman@gmail.com>
2021-04-19 17:15:17 +0600

    add desktop view for photo-card

--- components/PhotoCard.js
+++ components/PhotoCard.js
@@ -9,7 +9,7 @@ const PhotoCard = ({
 }) => {
   const renderAuthorBlock = () => (
       <div className="picture-card__author author">
-        <p className="author__avatar-wrap">
+        <p className="author__avatar-wrap author__avatar-wrap--card">
           <picture >
               <source media="(min-width: 1560px)" srcSet={authorInfo.avatarPath.desktopView} />
               <img className="author__avatar" src={authorInfo.avatarPath.mobileView} alt={authorInfo.alt} />
@@ -40,6 +40,7 @@ const PhotoCard = ({
         <svg className="picture-card__icon picture-card__icon--download" width="20" height="26" viewBox="0 0 20 26" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" clipRule="evenodd" d="M14.25 9.45837H16.5025C17.7633 9.45837 18.3867 10.9884 17.4942 11.8809L10.9917 18.3834C10.4392 18.9359 9.54668 18.9359 8.99418 18.3834L2.49168 11.8809C1.59918 10.9884 2.23668 9.45837 3.49751 9.45837H5.75001V2.37504C5.75001 1.59587 6.38751 0.958374 7.16668 0.958374H12.8333C13.6125 0.958374 14.25 1.59587 14.25 2.37504V9.45837ZM1.50001 25.0417C0.720844 25.0417 0.0833435 24.4042 0.0833435 23.6251C0.0833435 22.8459 0.720844 22.2084 1.50001 22.2084H18.5C19.2792 22.2084 19.9167 22.8459 19.9167 23.6251C19.9167 24.4042 19.2792 25.0417 18.5 25.0417H1.50001Z" />
         </svg>
+        <span className="picture-card__button-text">Download</span>
       </button>
     </div>
   );
@@ -130,12 +131,8 @@ const PhotoCard = ({
   };
 
   const renderPictureItem = () => {
-    const { jpg: defaultBgPhotoCardPath } = pictureItem.path.desktopView;
     const { desktopView: defaultImgPhotoCardPaths } = pictureItem.path;
-    const {
-      bgPhotoCard = defaultBgPhotoCardPath,
-      imgPhotoCard = defaultImgPhotoCardPaths,
-    } = pictureItem.path.desktopView;
+    const { imgPhotoCard = defaultImgPhotoCardPaths } = pictureItem.path.desktopView;
     return (
       <p className="picture-card__image-wrap">
         <picture >
@@ -172,18 +169,35 @@ const PhotoCard = ({
   };
 
   const { tags = null } = pictureItem;
+  const { jpg: defaultBgPhotoCardPath } = pictureItem.path.desktopView;
+  const { bgPhotoCard = defaultBgPhotoCardPath } = pictureItem.path.desktopView;
 
   return (
     <>
       <section className="page-main__picture-card picture-card">
         <div className="container container--fix">
           <h2 className="picture-card__title visually-hidden">Карточка картинки</h2>
-          <div className="picture-card__wrap">
-            {renderAuthorBlock()}
-            {renderActions()}
-          </div>
-          {renderPictureItem()}
+            <div className="picture-card__wrap">
+              {renderAuthorBlock()}
+              {renderActions()}
+            </div>
+            {renderPictureItem()}
         </div>
+        <style jsx>{`
+          @media (min-width: 1560px) {
+            .picture-card {
+              min-height: 1070px;
+              padding-bottom: 186px;
+
+              background-image:
+                linear-gradient(0deg, rgba(40, 36, 22, 0.6), rgba(40, 36, 22, 0.6)),
+                url(${bgPhotoCard});
+              background-repeat: no-repeat;
+              background-position: center;
+              background-size: cover;
+            }
+          }
+        `}</style>
       </section>
       <section className="page-main__tags tags">
         <div className="container container--fix">
